0951daf2e2614788f15c9189fe73a066db03f09a
Namrata157 <namratam157@gmail.com>
2023-07-30 15:58:22 +0530

    new commit

---
+++ src/Practice/Sample1Test2.java
@@ -0,0 +1,11 @@
+package Practice;
+
+public class Sample1Test2 {
+
+	public static void main(String[] args) {
+		System.out.println("This is my 2nd test");
+		System.out.println("This is addition");
+
+	}
+
+}
