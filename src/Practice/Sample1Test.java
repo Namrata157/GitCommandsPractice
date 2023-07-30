0230bdfaa35dddb0f253e777671827d6be173077
Namrata157 <namratam157@gmail.com>
2023-07-30 11:08:10 +0530

    second commit

--- src/Practice/Sample1Test.java
+++ src/Practice/Sample1Test.java
@@ -3,7 +3,8 @@ package Practice;
 public class Sample1Test {
 
 	public static void main(String[] args) {
-		System.out.println("This is my first script");
+		System.out.println("This is my first step");
+		System.out.println("This is my second step");
 
 	}
 
