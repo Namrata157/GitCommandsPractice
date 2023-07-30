189abe13a7a4802700011e1af3ebbd33cf723c52
Namrata157 <namratam157@gmail.com>
2023-07-30 16:08:45 +0530

    New branch commit

--- src/Practice/Sample1Test2.java
+++ src/Practice/Sample1Test2.java
@@ -3,7 +3,7 @@ package Practice;
 public class Sample1Test2 {
 
 	public static void main(String[] args) {
-		System.out.println("This is my 3rd test");
+		System.out.println("This is my 4th test");
 		
 	}
 
---
+++ src/Practice/Sample1Test4.java
@@ -0,0 +1,10 @@
+package Practice;
+
+public class Sample1Test4 {
+
+	public static void main(String[] args) {
+		System.out.println("This is my 3rd test");
+		
+	}
+
+}
